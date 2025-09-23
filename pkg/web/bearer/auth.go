48bce95b50395c44c3e0459a4167d24b2a474f6d
Patrick Vogt <patrick.vogt@unibas.ch>
2025-09-23 16:31:25 +0200

    write proper http code if unauthorised

--- pkg/web/bearer/auth.go
+++ pkg/web/bearer/auth.go
@@ -1,7 +1,6 @@
 package bearer
 
 import (
-	"log/slog"
 	"net/http"
 )
 
@@ -34,6 +33,9 @@ func (ta tokenAuth) Authorise(w http.ResponseWriter, r *http.Request) bool {
 		return true
 	}
 	t, ok := readBearer(w, r)
-	slog.Warn(" auth check", "ok", ok, "t", t, "token", ta.token)
+	// slog.Warn(" auth check", "ok", ok, "t", t, "token", ta.token)
+	if t != ta.token {
+		http.Error(w, "Could not authorise request", http.StatusUnauthorized)
+	}
 	return ok && t == ta.token
 }
