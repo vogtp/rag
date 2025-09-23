1dc27da447d3141f64ca0f5b563d636a57458a55
Patrick Vogt <patrick.vogt@unibas.ch>
2025-09-23 14:22:57 +0200

    better logging

--- cmd/web_test.go
+++ cmd/web_test.go
@@ -12,6 +12,8 @@ import (
 func Test_WebServer(t *testing.T) {
 	viper.AddConfigPath("..")
 	cfg.Parse()
+	viper.Set(cfg.LogJson, true)
+	viper.Set(cfg.LogLevel, "warn")
 	logger.New()
 
 	if err := startWeb(t.Context()); err != nil && err != http.ErrServerClosed {
