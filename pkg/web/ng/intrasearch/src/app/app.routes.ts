6596ff853d550fdbbb5faeb164b95b846ef6c981
Patrick Vogt <patrick.vogt@unibas.ch>
2025-09-23 13:45:31 +0200

    add settings to ui

--- pkg/web/ng/intrasearch/src/app/app.routes.ts
+++ pkg/web/ng/intrasearch/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CollectionsListComponent } from './components/collections-list/collections-list.component';
 import { SearchComponent } from './components/search/search.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: 'search', component: CollectionsListComponent },
   { path: 'search/:collection', component: SearchComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '', redirectTo: '/search', pathMatch: 'full' },
 ];
--- pkg/web/ng/intrasearch/src/app/components/settings-button/settings-button.component.ts
+++ pkg/web/ng/intrasearch/src/app/components/settings-button/settings-button.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings-button',
@@ -19,4 +19,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './settings-button.component.html',
   styleUrl: './settings-button.component.css',
 })
-export class SettingsButtonComponent {}
+export class SettingsButtonComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // go() {
+  //   this.router.navigate([`../edit/${item.id}`], { relativeTo: this.route });
+  // }
+
+  onSettingsClick() {
+    console.log('Settings');
+    this.router.navigate([`/settings`]);
+  }
+
+  onMenuItemClick(option: string) {
+    console.log('Selected:', option);
+  }
+}
