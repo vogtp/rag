2a8d7d6a3b359c6cc8cebc02da98b326108c5947
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-07 15:57:03 +0200

    rename

--- pkg/web/ng/intrasearch/src/app/components/collection-overview/collection-overview.component.ts
+++ pkg/web/ng/intrasearch/src/app/components/collection-overview/collection-overview.component.ts
@@ -1,18 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ChatbotComponent } from '../chat/chatbot.component';
-import { ChatbotIcons } from '../chat/interfaces/library.interface';
 import { SearchComponent } from '../search/search.component';
 
 @Component({
-  selector: 'app-collection',
+  selector: 'app-collection-overview',
+  standalone: true,
   imports: [ChatbotComponent, MatTabsModule, SearchComponent],
-  templateUrl: './collection.component.html',
-  styleUrl: './collection.component.css',
+  templateUrl: './collection-overview.component.html',
+  styleUrl: './collection-overview.component.css',
 })
-export class CollectionComponent {
+export class CollectionOverviewComponent {
   @Input() collection: string = 'intranet-all';
 
-  
   basePath: string = 'http://localhost:4444/api/chat/completions';
 }
