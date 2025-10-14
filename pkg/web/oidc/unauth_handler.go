7d50bf1fa65781fe0ebe105ab8933bb5b8afe07e
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-14 11:22:18 +0200

    log

--- pkg/web/oidc/unauth_handler.go
+++ pkg/web/oidc/unauth_handler.go
@@ -5,6 +5,6 @@ import (
 )
 
 func (om *mux) oidcUnauthHandler(w http.ResponseWriter, r *http.Request, desc string, state string) {
-	om.slog.Warn("OIDC unauthorised", "desc", desc, "state", state)
+	om.slog.Warn("OIDC unauthorised", "desc", desc, "state", state, "url", r.URL.String())
 	http.Redirect(w, r, om.loginPath, http.StatusTemporaryRedirect)
 }
