b60acae0c48ecad2146fe282eb183db13ea53445
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-14 15:24:07 +0200

    remove ent test stuff

--- cmd/db.go
+++ cmd/db.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"entgo.io/ent/dialect"
@@ -9,8 +8,6 @@ import (
 	"github.com/vogtp/rag/pkg/logger"
 	"github.com/vogtp/rag/pkg/usercfg"
 	"github.com/vogtp/rag/pkg/usercfg/db"
-	"github.com/vogtp/rag/pkg/usercfg/db/ent"
-	"github.com/vogtp/rag/pkg/usercfg/db/ent/sourcesystem"
 	"github.com/vogtp/rag/pkg/usercfg/db/ent/user"
 )
 
@@ -18,9 +15,7 @@ func addDB() {
 	rootCmd.AddCommand(dbCmd)
 	dbCmd.AddCommand(dbGormCmd)
 	dbCmd.AddCommand(dbUserCmd)
-	dbCmd.AddCommand(dbAddCmd)
 	dbCmd.AddCommand(dbCleanupCmd)
-	dbCmd.AddCommand(dbTestCmd)
 }
 
 var dbCmd = &cobra.Command{
@@ -125,113 +120,6 @@ var dbCleanupCmd = &cobra.Command{
 				return err
 			}
 		}
-		return nil
-	},
-}
-
-var dbAddCmd = &cobra.Command{
-	Use:   "add <username>",
-	Short: "add users to the DB",
-
-	Aliases:      []string{},
-	Long:         ``,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		db, err := usercfg.New(cmd.Context(), logger.New(), usercfg.Dialect, usercfg.DBFileName)
-		if err != nil {
-			return err
-		}
-		if len(args) < 1 {
-			return cmd.Usage()
-		}
-
-		srcConflPub, err := db.SourceSystem.Create().SetName("Intranet").SetURL("https://intranet-dev.unibas.ch/").SetParts("HR,KB").SetKey("xxxx").SetType(sourcesystem.TypeConfluence).Save(ctx)
-		if err != nil {
-			return err
-		}
-		srcHttplPub, err := db.SourceSystem.Create().SetName("Website").SetURL("https://its.unibas.ch/").SetParts("HR,KB").SetType(sourcesystem.TypeHTTP).Save(ctx)
-		if err != nil {
-			return err
-		}
-		srcConflPriv, err := db.SourceSystem.Create().SetName("Intranet").SetURL("https://intranet-dev.unibas.ch/").SetParts("mySpace,KB").SetKey("xxxx").SetType(sourcesystem.TypeConfluence).Save(ctx)
-		if err != nil {
-			return err
-		}
-		colPub, err := db.Collection.Create().SetName("Public collection").AddSources(srcConflPub, srcHttplPub).SetAPIKey("myPublicOpenaiAPIkey").Save(ctx)
-		if err != nil {
-			return err
-		}
-		colPriv, err := db.Collection.Create().AddSources(srcConflPriv).SetName("Private collection").SetAPIKey("mySecretOpenaiAPIkey").Save(ctx)
-		if err != nil {
-			return err
-		}
-		u, err := db.User.Create().SetName(args[0]).AddCollections(colPub, colPriv).OnConflict().UpdateNewValues().ID(ctx)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Created user: %v\n", u)
-
-		return nil
-	},
-}
-var tmp = `{
-  "id": 8589934593,
-  "Name": "vogtpa",
-  "OpenaiAPIkey": "MyCoolKey",
-  "edges": {
-    "Confluence": [
-      {
-        "id": 1,
-        "Name": "Intra",
-        "URL": "https://intranet-dev.unibas.ch/",
-        "APIKey": "yyyy",
-        "edges": {
-          "Spaces": [
-            {
-              "id": 4294967297,
-              "Name": "Our space",
-              "SpaceKey": "MsS"
-            }
-          ]
-        }
-      }
-    ]
-  }
-}`
-
-var dbTestCmd = &cobra.Command{
-	Use: "test",
-
-	Aliases:      []string{},
-	Long:         ``,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		db, err := usercfg.New(cmd.Context(), logger.New(), usercfg.Dialect, usercfg.DBFileName)
-		if err != nil {
-			return err
-		}
-		var usr ent.User
-		if err := json.Unmarshal([]byte(tmp), &usr); err != nil {
-			return err
-		}
-
-		u, err := db.User.Query().Where(user.ID(usr.ID)).First(ctx)
-		if err != nil {
-			return err
-		}
-		if err := json.Unmarshal([]byte(tmp), &u); err != nil {
-			return err
-		}
-		fmt.Printf("Marsh user: %v\n", u)
-		// u, err = db.User.UpdateOne(u).SetUser(u).AddConfluence(u.Confluence()...).Save(ctx)
-		// if err != nil {
-		// 	return err
-		// }
-		fmt.Printf("Updated user: %v\n", u)
-
 		return nil
 	},
 }
