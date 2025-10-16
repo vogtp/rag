d4cb6fee672278350d5066e013201d6132e8ee5d
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-16 13:35:43 +0200

    v0.5.0

--- pkg/cfg/version.go
+++ pkg/cfg/version.go
@@ -6,9 +6,9 @@ const (
 	// VersionMajor major version
 	VersionMajor = 0
 	// VersionMinor minor version
-	VersionMinor = 4
+	VersionMinor = 5
 	// VersionPatch patch level
-	VersionPatch = 3
+	VersionPatch = 0
 )
 
 var (
