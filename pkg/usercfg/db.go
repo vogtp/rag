b8aa41a509055ca39dc2b0b23c1b1a1215de9cb6
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-16 16:43:47 +0200

    give sources it's own file too...

--- pkg/usercfg/db.go
+++ pkg/usercfg/db.go
@@ -1,13 +1,9 @@
 package usercfg
 
-//go:generate stringer -type=SourceSystemType --trimprefix Source
-
 import (
 	"context"
 	"fmt"
 	"log/slog"
-	"strings"
-	"time"
 
 	slogGorm "github.com/orandin/slog-gorm"
 	"github.com/vogtp/rag/pkg/logger"
@@ -21,39 +17,6 @@ const (
 	//Dialect    = dialect.SQLite
 	DBFileName = "rag_users.sqlite"
 )
-
-type SourceSystemType int
-
-const (
-	SourceConfluence SourceSystemType = iota
-	SourceHTTP
-)
-
-type SourceSystem struct {
-	// gorm.Model in order to avoid name clashes
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-
-	CollectionID uint             `gorm:"column:collection_id"`
-	Name         string           `gorm:"column:name"`
-	Type         SourceSystemType `gorm:"column:src_sys_type"`
-	URL          string           `gorm:"column:url"`
-	Key          string           `gorm:"column:key"`
-	Parts        string           `gorm:"column:parts"`
-}
-
-func (s SourceSystem) splitParts() []string {
-	parts := strings.Split(s.Parts, ",")
-	if len(parts) < 1 {
-		parts = strings.Split(s.Parts, " ")
-	}
-	for i, p := range parts {
-		parts[i] = strings.TrimSpace(p)
-	}
-	return parts
-}
 
 type DataBase struct {
 	db   *gorm.DB
