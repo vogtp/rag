c81d5da18f2a322b2ea616f70bbb6cd6dd9849df
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-17 09:40:39 +0200

    cleanup

--- pkg/vecDB/confluence/confl_embedd.go
+++ pkg/vecDB/confluence/confl_embedd.go
@@ -2,7 +2,6 @@ package confluence
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log/slog"
 	"strings"
@@ -13,7 +12,6 @@ import (
 )
 
 func Embed(ctx context.Context, slog *slog.Logger, config cfg.RagConfig) error {
-	collectionName := config.CollectionName()
 	client, err := vecdb.New(ctx, slog, config.ModelEmbedding(), vecdb.WithOllamaAddress(cfg.GetOllamaHost(ctx)))
 	if err != nil {
 		return fmt.Errorf("Failed to create vector DB: %w", err)
@@ -28,47 +26,19 @@ func Embed(ctx context.Context, slog *slog.Logger, config cfg.RagConfig) error {
 		if err != nil {
 			return err
 		}
-		// o1, o2 := fanOut(c)
-
-		// go func() {
-		// 	colName := fmt.Sprintf("%s-%s", collectionName, "all")
-		// 	cfg := config
-		// 	cfg.Vecdb.CollectionName = colName
-		// 	slog := slog.With("collectionName", colName, "space", "all")
-		// 	slog.Info("Starting confluence embdding")
-		// 	if err := embedd(ctx, client, cfg, o1); err != nil {
-		// 		slog.Warn("Embedding returned an error", "err", err)
-		// 	}
-		// }()
-		// go func() {
-		colName := fmt.Sprintf("%s-%s", collectionName, strings.ToLower(space))
-		cfg := config
-		// cfg.Vecdb.CollectionName = colName
-		slog := slog.With("collectionName", colName, "space", space)
+		slog := slog.With("space", space)
 		slog.Info("Starting confluence embdding")
-		if err := embedd(ctx, client, cfg, c); err != nil {
+		start := time.Now()
+		cnt, err := client.Embedd(ctx, config, c)
+		if err != nil {
 			slog.Warn("Embedding returned an error", "err", err)
 		}
-		// }()
+		d := time.Since(start)
+		slog.Info("Embebbing finished", "document.count", cnt, "duration", d.String(), "duration_ms", d.Milliseconds())
+
 		if ctx.Err() != nil {
 			return ctx.Err()
 		}
 	}
-	return nil
-}
-
-func embedd(ctx context.Context, client *vecdb.VecDB, config cfg.RagConfig, c chan *vecdb.EmbeddDocument) error {
-	slog := slog.With("collection", config.CollectionName())
-	slog.Info("Embebbing start")
-	start := time.Now()
-	cnt, err := client.Embedd(ctx, config, c)
-	if errors.Is(err, context.Canceled) {
-		err = nil
-	}
-	if err != nil {
-		return fmt.Errorf("confluence embedding failed: %w", err)
-	}
-	d := time.Since(start)
-	slog.Info("Embebbing finished", "document.count", cnt, "duration", d.String(), "duration_ms", d.Milliseconds())
 	return nil
 }
