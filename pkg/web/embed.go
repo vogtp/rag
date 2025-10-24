32deafe46b24a2576abf7ad70d9f3e7564d6b687
Patrick Vogt <patrick.vogt@unibas.ch>
2025-10-24 16:10:45 +0200

    hint for next time

--- pkg/web/embed.go
+++ pkg/web/embed.go
@@ -5,8 +5,8 @@ import (
 	"html/template"
 )
 
-
 var (
+	// if ng-build fails ng/intrasearch/dist/intrasearch/browser may contain no files
 	//go:embed templates static ng/intrasearch/dist/intrasearch/browser
 	assetData embed.FS
 	templates = template.Must(template.ParseFS(assetData, "templates/*.gohtml", "templates/common/*.gohtml"))
