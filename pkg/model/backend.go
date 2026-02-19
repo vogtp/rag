2b3193d9b1949f92e87e37f269460dfca13298ec
Patrick Vogt <patrick.vogt@unibas.ch>
2026-02-19 16:49:47 +0100

    remove stacktrace

--- pkg/model/backend.go
+++ pkg/model/backend.go
@@ -64,7 +64,7 @@ func getOpenAIClient(_ context.Context, slog *slog.Logger, bm *cfg.BackendModel)
 		llmopenai.WithBaseURL(bm.API.URL),
 	}
 	if rc := bm.API.RateLimitedHTTPClient(); rc != http.DefaultClient {
-		slog.Info("Using ratelimiting http client", "RequestsPerSec", bm.API.Requests_per_sec, logger.Stacktrace())
+		slog.Info("Using ratelimiting http client", "RequestsPerSec", bm.API.Requests_per_sec)
 		opts = append(opts, llmopenai.WithHTTPClient(rc))
 	}
 	if len(bm.API.Key) > 0 {
